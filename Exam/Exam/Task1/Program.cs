e0e0851d2e384bb8ccfbf44ec37b88cd0850eb76
Georgi2423 <georged.94@abv.bg>
2015-11-12 21:17:02 +0200

    EXAm

--- Exam/Exam/Task1/Program.cs
+++ Exam/Exam/Task1/Program.cs
@@ -11,34 +11,43 @@ namespace Task1
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
-            int[,] arr2 = new int[n, n];
-            int[] arr1 = new int[n];
+            int[] arr1 = new int[n*n];
+            int[,] arr2 = new int[n,n];
+            int counter = 0;
             int sum = 0;
             Random rand = new Random();
 
-            
+
             for (int i = 0; i < arr1.Length; i++)
             {
-                arr1[i] = rand.Next(0,9);
+                arr1[i] = rand.Next(0, 9);
+                sum += arr1[i];
                 Console.WriteLine("{0}\t", arr1[i]);
             }
             Console.WriteLine();
-          
+
             for (int i = 0; i < arr2.GetLength(0); i++)
             {
                 for (int j = 0; j < arr2.GetLength(1); j++)
                 {
-                    arr2[i,j] = arr1[i];
-                    sum += arr2[j,i];
-                    Console.Write("{0}\t", arr2[j,i]);
+                    arr2[i,j] = arr1[counter++];
+         
+                }
+                
+
+            }
+            for (int i = 0; i < arr2.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr2.GetLength(1); j++)
+                {
+                   Console.Write("{0}\t", arr2[j, i]);
                 }
                 Console.WriteLine();
             }
-            
+
             Console.WriteLine("The sum is: {0}", sum);
-            
-           
+
+
         }
     }
 }
